b9ea2884a249606ce4cf43e89521b0ee6e32ae85
chloeechoi <55692774+chloeechoi@users.noreply.github.com>
2020-07-31 15:15:40 +0900

    7/31 db업로드 (dbwork0731 > songs)

--- week3_homework2.py
+++ week3_homework2.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
 
 client = MongoClient('localhost', 27017) # mongoDB는 27017 포트로 돌아갑니다.
-db = client.db명칭 # 'dbsparta'라는 이름의 db를 사용합니다. 'dbsparta' db가 없다면 새로 만듭니다.
+db = client.work0731 # 'dbsparta'라는 이름의 db를 사용합니다. 'dbsparta' db가 없다면 새로 만듭니다.
 
  # 타겟 URL을 읽어서 HTML를 받아오고,
 headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
@@ -21,5 +21,9 @@ for tr in trs:
     count += 1
     title = tr.select_one('td.info > a.title.ellipsis').text.strip()
     name = tr.select_one('td.info > a.artist.ellipsis').text
-
-    print(count, title, name)
+    doc = {
+        'rank' : count,
+        'title': title,
+        'name' : name
+    }
+    db.songs.insert_one(doc)
